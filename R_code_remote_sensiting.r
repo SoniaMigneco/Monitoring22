1ccf34b6cf934748647b6d29058afa77e9453b43
SoniaMigneco <92714028+SoniaMigneco@users.noreply.github.com>
2021-10-25 11:04:06 +0200

    Create R_code_remote_sensiting.r

---
+++ R_code_remote_sensiting.r
@@ -0,0 +1,8 @@
+# R code ecosystems monitoring by remote sensing 
+# First at all, we need to install additional package 
+# raster package to manege imagine data 
+# https://cran.r-project.org/web/packages/raster/index.html
+
+install.packages("raster")
+
+library(raster)
