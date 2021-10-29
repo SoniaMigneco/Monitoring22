f7d4de2fec28d86ebb53e1cef5638f03e4c57042
SoniaMigneco <92714028+SoniaMigneco@users.noreply.github.com>
2021-10-29 12:05:55 +0200

    Update R_code_remote_sensiting.r

--- R_code_remote_sensiting.r
+++ R_code_remote_sensiting.r
@@ -6,3 +6,21 @@
 install.packages("raster")
 
 library(raster)
+
+# setwd("/Users/name/lab/")
+
+#We are going to import satellite data
+l2011<- brick("p224r63_2011.grd")
+
+l2011
+
+plot(l2011)
+
+# B1 is the reflectance in the blue band 
+# B2 is the reflectance in the green band 
+# B3 is the reflectance in the red band 
+
+colorRampPalette(c("black", "grey", "light grey")) (100)
+
+cl <- colorRampPalette(c("black","grey","light grey"))(100)
+plot(l2011, col=cl)
